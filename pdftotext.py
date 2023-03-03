799ad25b3f283916de46af0fb5e0704c377ad935
merlos <jmerlostevar@unicef.org>
2023-03-03 15:05:24 +0300

    pdftotext, consistent naming, added validations

--- pdftotext.py
+++ pdftotext.py
@@ -12,32 +12,41 @@ import PyPDF2
 def main():
     # Define command line arguments
     parser = argparse.ArgumentParser(description='Convert PDF files to plain text')
-    parser.add_argument('dir_path', type=str, help='Path to directory containing PDF files')
-    parser.add_argument('-o', '--output', type=str, help='Path to output directory')
+    parser.add_argument('input_path', type=str, help='Path to directory containing PDF files')
+    parser.add_argument('-o', '--output', type=str, help='Path to output folder where txt files will be written')
 
     # Parse command line arguments
     args = parser.parse_args()
-    dir_path = args.dir_path
-    output_base_path = args.output or dir_path  # Use dir_path as output_path if not specified
+    input_path = args.input_path
+    output_path = args.output or input_path  # Use input_path as output_path if not specified
+
+
+    # If input path does not exist -> stop
+    if not os.path.exists(input_path):
+        parser.error(f"input_path directory '{input_path}' does not exist.")
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+        print(f"Created  output directory: {output_path}")
 
     # Iterate over PDF files in directory
-    for file_name in os.listdir(dir_path):
+    for input_file_name in os.listdir(input_path):
         #print(f'*** {file_name}')
-        if file_name.endswith('.pdf'):
+        if input_file_name.endswith('.pdf'):
             # Create input and output file paths
-            input_path = os.path.join(dir_path, file_name)
-            output_file_name = file_name[:-4] + '.txt'
-            output_path = os.path.join(output_base_path, output_file_name)
-            #print(f'**** dir path {dir_path} | output_path {output_path} | ofn {output_file_name}')
+            input_file_path = os.path.join(input_path, input_file_name)
+            output_file_name = input_file_name[:-4] + '.txt'
+            output_file_path = os.path.join(output_path, output_file_name)
+            #print(f'**** input path {input_path} | output_path {output_path} | ofn {output_file_name}')
             # Convert PDF to plain text
-            with open(input_path, 'rb') as input_file, open(output_path, 'w') as output_file:
+            with open(input_file_path, 'rb') as input_file, open(output_file_path, 'w') as output_file:
                 reader = PyPDF2.PdfReader(input_file)
                 for page_num in range(len(reader.pages)):
                     page = reader.pages[page_num]
                     text = page.extract_text()
                     output_file.write(text)
 
-            print(f'{file_name} converted to {output_path}')
+            print(f'{input_file_name} converted to {output_file_path}')
 
 if __name__ == '__main__':
     main()
