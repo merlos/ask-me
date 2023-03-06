bede9e833464d16d3f3e84cf6eb75685d6111c46
merlos <jmerlostevar@unicef.org>
2023-03-06 23:19:51 +0300

    added typewriter effect to bot

--- static/ui.js
+++ static/ui.js
@@ -22,7 +22,7 @@ const msgerInput = get(".msger-input");
 const msgerChat = get(".msger-chat");
 
 
-msgerForm.addEventListener("submit", event => {
+msgerForm.addEventListener("submit", async event => {
   event.preventDefault();
   
   // Get msgText
@@ -32,6 +32,8 @@ msgerForm.addEventListener("submit", event => {
   msgerInput.value = "";
   
   // Submit question
+  const sleep = ms => new Promise(r => setTimeout(r, ms));
+  await sleep(200);
   toggleBotLoading()
   postQuestion(msgText).then((response) => {
     console.log(response); // JSON data parsed by `data.json()` call
@@ -74,6 +76,7 @@ function toggleBotLoading() {
 
 function appendMessage(name, img, side, text) {
   //   Simple solution for small apps
+    const id = 'c' + Date.now()
     const msgHTML = `
     <div class="msg ${side}-msg">
       <div class="msg-img" style="background-image: url(${img})"></div>
@@ -83,13 +86,27 @@ function appendMessage(name, img, side, text) {
           <div class="msg-info-name">${name}</div>
           <div class="msg-info-time">${formatDate(new Date())}</div>
         </div>
-        <div class="msg-text">${text}</div>
+        <div class="msg-text" id="${id}">${side === 'left' ? "" : text}</div>
       </div>
     </div>
   `;
 
   msgerChat.insertAdjacentHTML("beforeend", msgHTML);
   msgerChat.scrollTop += 500;
+
+  if (side === 'left') {
+    new TypeIt(`#${id}`, {
+      strings: text,
+      speed: 30, 
+      cursor: true,
+      afterStep: async () => {
+        msgerChat.scrollTop += 500; // this helps to scroll when a new line is added
+      },
+      afterComplete: async (instance) => {
+        instance.destroy() //remove cursor when done
+      }
+    }).go();
+  } 
 }
 
 function botResponse(msgText) {
